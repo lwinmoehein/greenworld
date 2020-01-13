5c97a151893494f59ee96bcd54cb3faa217358a1
lwinmoehein <dev.lwinmoehein@gmail.com>
2020-01-13 13:23:04 +0630

    add main navigation

--- app/src/main/java/trapleh/io/greenworld/MainActivity.java
+++ app/src/main/java/trapleh/io/greenworld/MainActivity.java
@@ -1,14 +1,37 @@
 package trapleh.io.greenworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+import trapleh.io.greenworld.fragment.MainFragment;
+import trapleh.io.greenworld.ui.TabAdapter;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabAdapter adapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        adapter = new TabAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new MainFragment(), "home");
+        adapter.addFragment(new MainFragment(), "plastic");
+        adapter.addFragment(new MainFragment(), "plants");
+        adapter.addFragment(new MainFragment(), "profile");
+
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
---
+++ app/src/main/java/trapleh/io/greenworld/fragment/MainFragment.java
@@ -0,0 +1,15 @@
+package trapleh.io.greenworld.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import trapleh.io.greenworld.R;
+
+public class MainFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
