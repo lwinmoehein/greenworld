6258bbef33414f49f41abda92eea28d15f912151
kywthiha <kyawthi547@gmail.com>
2020-01-15 01:03:45 +0630

    User profile connect firebase auth

--- app/src/main/java/trapleh/io/greenworld/activity/RegisterActivity.java
+++ app/src/main/java/trapleh/io/greenworld/activity/RegisterActivity.java
@@ -1,15 +1,21 @@
 package trapleh.io.greenworld.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -36,46 +42,35 @@ public class RegisterActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
         userId = user.getUid();
-        mDatabase.child(userId).addListenerForSingleValueEvent(
-                new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        // Get user value
-                        User user = dataSnapshot.getValue(User.class);
-
-                        // [START_EXCLUDE]
-                        if (user == null) {
-                            // User is null, error out
-
-                        } else {
-                            Intent intent=new Intent(getApplicationContext(), MainActivity.class);
-                            startActivity(intent);
-                            finish();
-
-                        }
-
-                        // [END_EXCLUDE]
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                        Log.w(TAG, "getUser:onCancelled", databaseError.toException());
-
-                    }
-                });
-        editTextName= findViewById(R.id.editTextName);
+        if (user.getDisplayName() != null){
+            Intent intent=new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        editTextName = findViewById(R.id.editTextName);
 
     }
 
 
     public void actionRegister(View view) {
-        User userTemp = new User();
-        userTemp.phoneNo = user.getPhoneNumber();
-        userTemp.username= editTextName.getText().toString();
-        mDatabase.child(userId).setValue(user);
-        Intent intent=new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
-        finish();
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(editTextName.getText().toString())
+                .setPhotoUri(Uri.parse("https://firebasestorage.googleapis.com/v0/b/greenplanet-62377.appspot.com/o/users%2Fman.png?alt=media&token=17e78b68-6077-4901-bc92-3af43bfe1835"))
+                .build();
+        user.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "User profile updated.");
+                            Toast.makeText(getApplicationContext(),"User profile updated.",Toast.LENGTH_SHORT).show();
+                            Intent intent=new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
+
 
     }
 }
--- app/src/main/java/trapleh/io/greenworld/statics/UserStatic.java
+++ app/src/main/java/trapleh/io/greenworld/statics/UserStatic.java
@@ -1,8 +1,11 @@
 package trapleh.io.greenworld.statics;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class UserStatic {
     public static DatabaseReference userRef= FirebaseDatabase.getInstance().getReference().child("users");
+    public static FirebaseUser  currentUser = FirebaseAuth.getInstance().getCurrentUser();
 }
