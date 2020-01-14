8d3fb1b3971822b9534ef239d9c16f019aef2b0b
lwinmoehein <dev.lwinmoehein@gmail.com>
2020-01-14 15:40:03 +0630

    firebase storage,post,database,picasso

---
+++ app/src/main/java/trapleh/io/greenworld/activity/NewPostActivity.java
@@ -0,0 +1,176 @@
+package trapleh.io.greenworld.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.UUID;
+
+import gun0912.tedbottompicker.TedBottomPicker;
+import gun0912.tedbottompicker.TedBottomSheetDialogFragment;
+import trapleh.io.greenworld.R;
+import trapleh.io.greenworld.helper.PermissionHelper;
+import trapleh.io.greenworld.model.Post;
+import trapleh.io.greenworld.statics.PostStatic;
+
+public class NewPostActivity extends AppCompatActivity {
+    private ImageView backArrow,selectedImage;
+    private TextView postButton;
+    private EditText postText;
+    private Button selectImage,postBtn;
+
+    private String strPostText;
+    private Uri uriPostImgPath;
+
+    ProgressDialog progressDialog;
+
+
+    DatabaseReference postReference= PostStatic.postRef;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_post);
+
+        backArrow=findViewById(R.id.post_back_arrow);
+        postButton=findViewById(R.id.post_btn);
+        selectImage=findViewById(R.id.selectImage);
+        postBtn=findViewById(R.id.uploadPost);
+        postText=findViewById(R.id.post_text);
+        selectedImage=findViewById(R.id.post_image);
+
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadPost();
+            }
+        });
+        postBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadPost();
+            }
+        });
+        selectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImg();
+            }
+        });
+    }
+
+    private void selectImg() {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                PermissionHelper.checkImgPickPermission(this);
+            }
+            TedBottomPicker.with(this)
+                    .setPeekHeight(1200)
+                    .setTitle("please pick an image")
+                    .showTitle(true)
+                    .setGalleryTileBackgroundResId(R.color.colorPrimary)
+                    .show(new TedBottomSheetDialogFragment.OnImageSelectedListener() {
+                        @Override
+                        public void onImageSelected(Uri uri) {
+                            selectedImage.setVisibility(View.VISIBLE);
+                            Picasso.get()
+                                    .load(uri)
+                                    .resize(300, 300)
+                                    .centerCrop()
+                                    .into(selectedImage);
+                            uriPostImgPath=uri;
+                         }
+                    });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Cannot Pick Image", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void uploadPost() {
+        progressDialog=new ProgressDialog(this);
+        progressDialog.setTitle("Uploading...");
+        progressDialog.show();
+        //prepare data
+
+        strPostText=postText.getText().toString();
+        if(uriPostImgPath!=null){
+            StorageReference childRef = PostStatic.postImageRef.child(UUID.randomUUID().toString()+".jpg");
+            childRef.putFile(uriPostImgPath).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double bytesTransferred = (double) taskSnapshot.getBytesTransferred();
+                    Double.isNaN(bytesTransferred);
+                    bytesTransferred *= 100.0d;
+                    double totalByteCount = (double) taskSnapshot.getTotalByteCount();
+                    Double.isNaN(totalByteCount);
+                    bytesTransferred /= totalByteCount;
+                    ProgressDialog progressDialog = new ProgressDialog(getApplicationContext());
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.append("Uploaded ");
+                    stringBuilder.append((int) bytesTransferred);
+                    stringBuilder.append("%");
+                    progressDialog.setMessage(stringBuilder.toString());
+
+                }
+            })
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            childRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    String id=postReference.push().getKey();
+                                    Post post=new Post(id,"uid","uname","profilepath",strPostText,true,uri.toString(),0,0,ServerValue.TIMESTAMP);
+                                    postReference.child(id).setValue(post);
+                                    Toast.makeText(getApplicationContext(),"Uploaded new Post",Toast.LENGTH_LONG).show();
+
+                                }
+                            })
+                            ;
+                        }
+                    });
+        }else {
+            String id=postReference.push().getKey();
+            Post post=new Post(id,"uid","uname","profilepath",strPostText,false,"",0,0,ServerValue.TIMESTAMP);
+            postReference.child(id).setValue(post)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Toast.makeText(getApplicationContext(),"Uploaded new Post",Toast.LENGTH_LONG).show();
+                    progressDialog.dismiss();
+                }
+            });
+
+
+
+        }
+
+    }
+}
--- app/src/main/java/trapleh/io/greenworld/fragment/MainFragment.java
+++ app/src/main/java/trapleh/io/greenworld/fragment/MainFragment.java
@@ -1,5 +1,6 @@
 package trapleh.io.greenworld.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,17 +12,29 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import trapleh.io.greenworld.MainActivity;
 import trapleh.io.greenworld.R;
+import trapleh.io.greenworld.activity.NewPostActivity;
 import trapleh.io.greenworld.adapter.PostAdapter;
 import trapleh.io.greenworld.model.Post;
+import trapleh.io.greenworld.statics.PostStatic;
 
 public class MainFragment extends Fragment {
     RecyclerView recyclerPosts;
+    FloatingActionButton addPost;
     PostAdapter adapterPosts;
     List<Post> posts=new ArrayList<>();
+
+    DatabaseReference postReference= PostStatic.postRef;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
@@ -31,16 +44,48 @@ public class MainFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         recyclerPosts=view.findViewById(R.id.recycler_posts);
-
-        posts.add(new Post("id","u","hola","po","adfadfa",true,3,3,"3.3.3"));
-        posts.add(new Post());
-        posts.add(new Post());
-        posts.add(new Post());
+        addPost=view.findViewById(R.id.add_post);
 
         adapterPosts=new PostAdapter(posts);
         recyclerPosts.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerPosts.setAdapter(adapterPosts);
 
+        addPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(),NewPostActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        postReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Post post=dataSnapshot.getValue(Post.class);
+                posts.add(post);
+                adapterPosts.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
     }
